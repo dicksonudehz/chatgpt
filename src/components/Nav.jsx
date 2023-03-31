e007074e52046997a351acdadc5f7bcceb3da47e
github_dicksonudehz <dicksonudehz.dicksonudehz@gmail.com>
2023-03-31 14:56:58 -0700

    commit to the github

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./nav.css";
-import illustrator from '../assets/headerillustrator.png'
-import googlelogo from '../assets/google.png'
-import bitmaplogo from '../assets/Bitmap.png'
-import dropboxlogo from '../assets/dropbox.png'
-import slacklogo from '../assets/slack.png'
-import shopifylogo from '../assets/shopify.png'
+import illustrator from "../assets/headerillustrator.png";
+import googlelogo from "../assets/google.png";
+import bitmaplogo from "../assets/Bitmap.png";
+import dropboxlogo from "../assets/dropbox.png";
+import slacklogo from "../assets/slack.png";
+import shopifylogo from "../assets/shopify.png";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Nav() {
   return (
     <div className="mainNavContentContainer">
-      <div className="navContainer">
+      
+      {/* <div className="navToogle"> */}
+          <MenuIcon className="navMenuIcon"/>
+        {/* </div> */}
+          <input type="checkbox" className="checkToogle" />
+          <div id="toogleContainer">
+      <div className="navContainer" id="navContainerCont">
         <div className="leftContainer">
           <ul>
             <li>GPT-3</li>
@@ -24,20 +31,29 @@ function Nav() {
         <div className="rightContainer">
           <span>sign in</span>
           <button className="signUpButton">sign up</button>
+          </div>
         </div>
       </div>
       <div className="navBodyContainer">
         <div className="leftBodyContainer">
-          <h1>let's build something <br />amazing with GPT-3<br /> openAi</h1>
+          <h1>
+            let's build something <br />
+            amazing with GPT-3
+            <br /> openAi
+          </h1>
           <p>
-            Yet bed any for travelling assistance indulgence unpleasing.
-            Not thoughts all exercise blessing. Indulgence way
-            everything joy alteration boisterous the attachment. Party
-            we years to order allow asked of.
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
           </p>
           <div className="signupContainer">
-          <input type="text" placeholder="your email address" className="inputClass"/>
-          <button className="getStarted">get started</button>
+            <input
+              type="text"
+              placeholder="your email address"
+              className="inputClass"
+            />
+            <button className="getStarted">get started</button>
           </div>
         </div>
         <div className="rightNavContainer">
